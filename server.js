1017158d592ca5df3cf40cf0f2966c9522d20325
Silvia Abe <silviaabebuzatto@gmail.com>
2024-11-23 13:18:27 -0300

    deploy do projeto

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from "express"
+import routes from "./src/routes/postsRoutes.js"
+
+const app = express()
+app.use(express.static("uploads"))
+routes(app)
+
+app.listen(3000, () => {
+  console.log("Servidor escutando...")
+})
+
+// function buscarPostPorId(id) {
+//   return posts.findIndex((post) => {
+//     return post.id === Number(id)
+//   })
+// }
+
+// app.get("/posts/:id", (req, res) => {
+//   const index = buscarPostPorId(req.params.id)
+//   res.status(200).json(posts[index])
+// })
