d5a9bce802d67bb1b0e637046263d3348153bb09
Silvia Abe <160606092+silviaabe@users.noreply.github.com>
2024-11-25 09:56:08 -0300

    atualizando

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-import express from "express"
-import routes from "./src/routes/postsRoutes.js"
+import express from "express";
+import routes from "./src/routes/postsRoutes.js";
 
-const app = express()
+const app = express();
 app.use(express.static("uploads"))
 routes(app)
 
+// Inicia o servidor na porta 3000 e exibe uma mensagem no console
 app.listen(3000, () => {
   console.log("Servidor escutando...")
-})
+});
 
 // function buscarPostPorId(id) {
 //   return posts.findIndex((post) => {
